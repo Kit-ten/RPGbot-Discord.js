dad4d2c040f265c3fa15192a4c071cc7ddf4a1d5
Kitten <66484733+Kit-ten@users.noreply.github.com>
2020-09-08 12:34:49 +0100

    Create profile.js

---
+++ models/profile.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const profileSchema = mongoose.Schema({
+  userID: String,
+  username: String,
+  serverID: String,
+  class: { type: String, default: 'None' },
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  traits: { type: [], default: [] },
+});
+
+module.exports = mongoose.model('Profile', profileSchema);
