68f4d5fb5c4f3bea2a96846977401e0b4cc5b7ec
Kitten <66484733+Kit-ten@users.noreply.github.com>
2020-09-08 13:59:04 +0100

    Create battle.js

---
+++ utils/battle.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const { mongoPass } = require('../config.json');
+
+mongoose.connect(mongoPass, {
+    useNewUrlParser: true,
+});
+const Profile = require('../models/profile.js');
+
+module.exports.recordSave = (message, winner, loser) => {
+    Profile.findOne({
+        userID: winner.id,
+        serverID: message.guild.id,
+    }, (err, res) => {
+        if (err) console.log(err);
+        if (!res) {
+            const newDoc = new Profile({
+                userID: winner.id,
+                username: winner.user.username,
+                serverID: message.guild.id,
+                wins: 1,
+                losses: 0,
+            });
+            newDoc.save().catch(err => console.log(err));
+        }
+ else {
+            res.wins = res.wins + 1;
+            res.save().catch(err => console.log(err));
+        }
+        Profile.findOne({
+            userID: loser.id,
+            serverID: message.guild.id,
+        }, (err, res) => {
+            if(err) console.log(err);
+            if(!res) {
+                const newDoc = new Profile({
+                    userID: loser.id,
+                    username: loser.user.username,
+                    serverID: message.guild.id,
+                    wins: 0,
+                    losses: 1,
+                });
+                newDoc.save().catch(err => console.log(err));
+            }
+else{
+                res.losses = res.losses + 1;
+                res.save().catch(err => console.log(err));
+            }
+        });
+    });
+};
