a4b68bbc5dbbdb0c6388e642c01031ae5c7439e0
Kitten <66484733+Kit-ten@users.noreply.github.com>
2020-09-08 12:33:46 +0100

    Create pets.js

---
+++ models/pets.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const petSchema = mongoose.Schema({
+    petName: String,
+    petTrait: { type: String, default: 'None' },
+    ownerID: String,
+    ownerUsername: String,
+    petLvl: { type: Number, default: 1 },
+    petXp: { type: Number, default: 0 },
+    petStats: { type: {}, default: { attack: 1, strength: 1, defence: 1, agility: 1 } },
+    petTier: { type: Number, default: 1 },
+    petRerolls: { type: Number, default: 0 },
+});
+module.exports = mongoose.model('Pets', petSchema);
